a3c09adf0c0bde73fd173e187b1bb6397e591d68
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-08-03 01:35:00 +1000

    Add Accounts collection

---
+++ imports/core/api/accounts/accounts.js
@@ -0,0 +1,15 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+import { createdAt, updatedAt, ethereumAddressType } from '../schema-helpers';
+
+export const Accounts = new Mongo.Collection('accounts');
+
+Accounts.schema = new SimpleSchema({
+  _id: ethereumAddressType,
+  balance: { type: SimpleSchema.Integer },
+  createdAt,
+  updatedAt,
+});
+
+Accounts.attachSchema(Accounts.schema);
