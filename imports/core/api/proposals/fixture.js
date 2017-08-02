db5df667316a2987376c839732c13c99c9750f3d
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-08-03 01:34:38 +1000

    Modify Proposals schema

--- imports/core/api/proposals/fixture.js
+++ imports/core/api/proposals/fixture.js
@@ -4,15 +4,9 @@ import { Proposals } from './proposals';
 
 Proposals.remove({});
 _.range(0, 100).forEach(i => {
-  const agreed = _.random(1000, 10000000);
-  const doubt = _.random(1000, 10000000);
-  const total = Math.abs(agreed - doubt);
   Proposals.insert({
-    title: `Proposal ${i}. Some long proposal text to be agreed or doubted`,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    total,
-    agreed,
-    doubt,
+    statement: `Proposal ${i}. Some long proposal text to be agreed or doubted`,
+    upVoteAmount: _.random(1000, 10000000),
+    downVoteAmount: _.random(1000, 10000000),
   });
 });
--- imports/core/api/proposals/proposals.js
+++ imports/core/api/proposals/proposals.js
@@ -1,27 +1,27 @@
-import SimpleSchema from 'simpl-schema'
-import { Mongo } from 'meteor/mongo'
+import SimpleSchema from 'simpl-schema';
+import { Mongo } from 'meteor/mongo';
+
+import {
+  createdAt, updatedAt,
+} from '../schema-helpers';
 
 export const Proposals = new Mongo.Collection('proposals');
 
-const proposalsType = new SimpleSchema({
-  title: {
-    type: 'String',
-  },
-  createdAt: {
-    type: Date,
-  },
-  updatedAt: {
-    type: Date,
-  },
-  total: {
-    type: Number,
-  },
-  agreed: {
-    type: Number,
-  },
-  doubt: {
-    type: Number,
-  },
-});
-
-Proposals.attachSchema(proposalsType);
+Proposals.attachSchema(new SimpleSchema({
+  _id: { type: String, regEx: SimpleSchema.RegEx.Id },
+  statement: { type: String },
+  upVoteAmount: { type: SimpleSchema.Integer, defaultValue: 0 },
+  downVoteAmount: { type: SimpleSchema.Integer, defaultValue: 0 },
+  votes: { type: Object, blackbox: true, defaultValue: {} },
+  /*
+  votes: {
+    accountId: { // ethereumAddressType
+      signature: ethereumSignatureType,
+      upVote: boolean,
+      createdAt: Date,
+    }
+  }
+   */
+  createdAt,
+  updatedAt,
+}));
