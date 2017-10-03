816b2b6b2d8f94912fb6d63b49608846644f5a19
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-10-03 15:58:51 +1000

    Add CopyButton component
    Based on 3f1f3b96b11fd5dd610c8b5382a0977b5fd3d145

---
+++ imports/voting/ui/particles/CopyButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    v-clipboard="contentToCopy"
+    :key="contentToCopy"
+    @success="label = 'Copied!'"
+    @error="label = 'Copy Error!'"
+    type="button"
+  >
+    {{label}}
+  </button>
+</template>
+
+<script>
+  export default {
+    props: ['contentToCopy'],
+    data() {
+      return {
+        label: 'Copy',
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "/imports/voting/ui/styles/variables";
+
+  button {
+    border: none;
+    text-decoration: underline;
+    color: $gray;
+  }
+</style>
