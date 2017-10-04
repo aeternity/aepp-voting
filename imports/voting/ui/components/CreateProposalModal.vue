61d5d98c95e9144b1a45c6b7189c26f093313f01
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-10-04 13:45:40 +1000

    Extract Modal component

--- imports/voting/ui/components/CreateProposalModal.vue
+++ imports/voting/ui/components/CreateProposalModal.vue
@@ -1,31 +1,24 @@
 <template>
-  <div class="modal">
-    <div class="overlay" @click="toggleCreateProposalModal"></div>
-    <div class="modal-cont">
-      <div class="modal-header">
-        <button @click="toggleCreateProposalModal">
-          <i class="fa fa-close" />
-        </button>
-      </div>
-      <div class="submit-proposal">
-        <h3>
-          Enter your statement and agree or disagree
-          <span>(please keep it short)</span>
-        </h3>
-        <label for="statement">Your statement</label>
-        <input id="statement" placeholder="Saturdays are great" v-model="statement" />
-        <sign-statement
-          :statement="statement || 'Saturdays are great'"
-          :signatureHandler="signatureHandler"
-        ></sign-statement>
-      </div>
+  <modal :closeHandler="toggleCreateProposalModal">
+    <div class="submit-proposal">
+      <h3>
+        Enter your statement and agree or disagree
+        <span>(please keep it short)</span>
+      </h3>
+      <label for="statement">Your statement</label>
+      <input id="statement" placeholder="Saturdays are great" v-model="statement" />
+      <sign-statement
+        :statement="statement || 'Saturdays are great'"
+        :signatureHandler="signatureHandler"
+      ></sign-statement>
     </div>
-  </div>
+  </modal>
 </template>
 
 <script>
   import { mapMutations } from 'vuex';
 
+  import Modal from '../particles/Modal.vue';
   import SignStatement from '../particles/SignStatement.vue';
   import { Proposals } from '../../api/models/proposals';
 
@@ -34,6 +27,7 @@
       return { statement: '' };
     },
     components: {
+      Modal,
       SignStatement,
     },
     methods: {
@@ -54,67 +48,28 @@
 <style lang="scss" scoped>
   @import "/imports/voting/ui/styles/variables";
 
-  .modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 100;
-    overflow: hidden;
-    .overlay {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      top: 0;
-      left: 0;
-      background: rgba(0, 0, 0, .7);
+  .submit-proposal {
+    h3 {
+      text-align: center;
+      margin-bottom: $gutter * 3;
+      span {
+        display: block;
+        font-weight: normal;
+        font-size: 0.9em;
+      }
     }
-    .modal-cont {
-      z-index: 1;
-      width: 650px;
-      background: white;
-      border-radius: $base-border-radius;
-      box-shadow: $base-box-shadow;
-      padding: $gutter;
-      @media screen and (max-width: $container-width) {
-        max-width: 96vw;
-      }
-      .modal-header {
-        button {
-          float: right;
-          border: 0;
-          padding: 0;
-          color: black;
-        }
-      }
-      .submit-proposal {
-        h3 {
-          text-align: center;
-          margin-bottom: $gutter * 3;
-          span {
-            display: block;
-            font-weight: normal;
-            font-size: 0.9em;
-          }
-        }
-        > label, > input {
-          margin: $gutter auto;
-          width: 80%;
-          display: block;
-        }
-        > input {
-          height: 30px;
-          padding: 7px;
-          border-radius: 4px;
-          font-size: 24px;
-          border: solid 1px $gray-light;
-          box-shadow: none;
-        }
-      }
+    > label, > input {
+      margin: $gutter auto;
+      width: 80%;
+      display: block;
+    }
+    > input {
+      height: 30px;
+      padding: 7px;
+      border-radius: 4px;
+      font-size: 24px;
+      border: solid 1px $gray-light;
+      box-shadow: none;
     }
   }
 </style>
