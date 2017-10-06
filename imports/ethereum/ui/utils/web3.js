edae7b97010e9469a7745f6ded7b2f7f21d65ca9
Sascha Hanse <shanse@posteo.de>
2017-10-06 03:11:00 +0200

    removes sync web3.eth calls and checks the parent window for a web3 instance to get a provider from

--- imports/ethereum/ui/utils/web3.js
+++ imports/ethereum/ui/utils/web3.js
@@ -6,6 +6,8 @@ window.addEventListener('load', function() {
   if (window.web3) {
     web3.setProvider(window.web3.currentProvider);
     delete window.web3;
+  } else if (window.parent !== window && window.parent.web3 !== undefined) {
+    web3.setProvider(window.parent.web3.currentProvider);
   }
 });
 
