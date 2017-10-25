78e0266633e2f6bb237a7966739263353b6bf33e
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2017-10-25 23:50:35 +1000

    Add base aepp components

---
+++ imports/components/AeCloseButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="ae-close-button" @click="click">
+    <i class="fa" />
+  </button>
+</template>
+
+<script>
+  export default {
+    methods: {
+      click(e) {
+        this.$emit('click', e);
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .ae-close-button {
+    border: 0;
+    padding: 0;
+    font-size: 40px;
+    background: transparent;
+    width: 20px;
+    height: 20px;
+
+    i {
+      margin-top: -10px;
+      display: block;
+      &:before {
+        content: '\00d7';
+      }
+    }
+  }
+</style>
