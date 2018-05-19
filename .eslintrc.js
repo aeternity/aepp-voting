51b86ce9597dae3c70e8d476fda5d2fcf23dff1d
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-05-19 08:32:08 +0200

    Remove unnecessary eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,17 +17,12 @@ module.exports = {
     'meteor',
   ],
   settings: {
-    'import/core-modules': [
-      'meteor/meteor',
-      'meteor/tmeasday:publish-counts',
-    ],
     'import/resolver': 'meteor',
   },
   'rules': {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-underscore-dangle': [ 'error', { allow: [ '_id' ] } ],
     'import/no-absolute-path': 0,
-    'import/no-extraneous-dependencies': 0,
     'import/extensions': 0,
     'import/prefer-default-export': 0,
   }
